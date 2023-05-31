76de8c132c4e74c86ddf6aa572d06e8014aba688
maerany <88430300+maerany@users.noreply.github.com>
2023-06-01 07:23:02 +0900

    chapter03-3. posts등록하기 구현 (주석 추가)

--- src/test/java/com/maerany/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/maerany/book/springboot/web/PostsApiControllerTest.java
@@ -20,6 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// jpa기능까지 한번에 테스트할 때 사용하는 어노테이션 @SpringBootTest
+// @MevMvcTest는 jap기능이 작동하지 않고 controller, controllerAdvice 등 외부 연동과 관련된 부부만 활성화
 public class PostsApiControllerTest {
 
     @LocalServerPort
@@ -39,6 +41,10 @@ public class PostsApiControllerTest {
 
     @Test
     public void Posts_등록된다() throws Exception{
+        // 여기서 오류가 발생했었는데,
+        // 1. PostsApiController에 @PostMapping() 안해서
+        // 2. PostsSaveRequestDto에 @Getter / @NoArgsConstructor 안해서..
+        // 어노테이션에 대한 사용을 언제, 왜 하는지 확실히 익힐 필요가 있음..!
 
         //given
         String title = "title";
