7ecd24826f96b8bdc016ec89e2cb492dce07f81e
maerany <88430300+maerany@users.noreply.github.com>
2023-06-07 16:01:23 +0900

    Revert "chapter05-3. @LoginUser 어노테이션 생성 (HandlerMethodArgumentResolver)"
    This reverts commit 5cfd5819a29de5c47fd9853826b68cd03985dc48.

--- src/main/java/com/maerany/book/springboot/web/dto/IndexController.java
+++ src/main/java/com/maerany/book/springboot/web/dto/IndexController.java
@@ -1,6 +1,5 @@
 package com.maerany.book.springboot.web.dto;
 
-import com.maerany.book.springboot.config.auth.LoginUser;
 import com.maerany.book.springboot.config.auth.dto.SessionUser;
 import com.maerany.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +18,7 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user){
+    public String index(Model model){
         /*
         *  mustache 스타터로 앞의 경로와 뒤의 확장자는 자동으로 지정됨
         *  src/main/resources/template/ + "index" + .mustache
@@ -34,13 +33,9 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
 
-        /*
-            @LoginUser Annotation을 이용함으로써 해당 코드 중복 제거 및 유지보수성 높힘
-            // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
-               SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
-           - @LoginUser를 사용하면 어느 컨트롤러든지 해당 어노테이션을 통해 세션 정보를 가져올 수 있다.
-        */
         // Session에 저장된 값이 있을 경우에만 model에 userName을 등록
         if(user != null){
             model.addAttribute("userName", user.getName());
