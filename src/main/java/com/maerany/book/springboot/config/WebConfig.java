abf5caeecf85eac38642d47874375522648584d4
maerany <88430300+maerany@users.noreply.github.com>
2023-06-07 18:37:07 +0900

    chapter05-3. @LoginUser 어노테이션 생성(재커밋 추가)

---
+++ src/main/java/com/maerany/book/springboot/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.maerany.book.springboot.config;
+
+import com.maerany.book.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    /*
+        HandlerMethodArgumentResolver은 항상 WebMvcConfigurer의 AddArgumentResolvers()를 통해 추가해야 함
+        다른 HandlerMethodArgumentResolver가 필요하다면 이와 같은 방식으로 추가해주면 됨.
+     */
+}
--- src/main/java/com/maerany/book/springboot/config/auth/LoginUser.java
+++ src/main/java/com/maerany/book/springboot/config/auth/LoginUser.java
@@ -1,4 +1,25 @@
 package com.maerany.book.springboot.config.auth;
 
-public interface LoginUser {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+
+    /*
+        1. @Target(ElementType.PARAMETER)
+        - 해당 어노테이션이 생성될 수 있는 위치 생성
+        - PARAMETER로 지정했으므로 메소드의 파라미터로 선언된 객체에서만 사용
+        - 이 외에도 class 선언문에 쓸 수 있는 Type 등이 있음
+
+        2. @interface
+        - 해당 파일을 어노테이션 클래스로 지정
+        - LoginUser라는 이름을 가진 어노테이션이 생성되었다고 볼 수 있음
+     */
+
 }
+
+
